715ab7d4975008b61ddc7627037c30ef74165023
vadymkostyuk <kostiukvadym22@gmail.com>
2020-04-24 18:57:48 +0300

    add first task hw_6

--- FL13_HW6/homework/js/task1.js
+++ FL13_HW6/homework/js/task1.js
@@ -1,1 +1,15 @@
-// Your code goes here
+const maxVal = 100;
+const round = 2;
+const check = +parseFloat(prompt('Please enter check number', '0')).toFixed(round);
+const percent = +parseFloat(prompt('Please enter tip percentage', '0')).toFixed(round);
+const amount = +(check * percent / maxVal).toFixed(round);
+const sum = +(check + amount).toFixed(round);
+if(percent <= maxVal && percent > 0 && check > 0) {
+    alert(`Check number: ${check}
+Tip: ${percent}%
+Tip amount: ${amount}
+Total sum to pay:  ${sum}`);
+} else {
+    alert('Invalid input data')
+}
+
