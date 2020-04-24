5cf4921183a278624b6c31e22f9b00ea660d8d83
vadymkostyuk <kostiukvadym22@gmail.com>
2020-04-24 20:45:08 +0300

    hw_6 complete

--- FL13_HW6/homework/js/task2.js
+++ FL13_HW6/homework/js/task2.js
@@ -1,1 +1,10 @@
-// Your code goes here
+const userText = prompt('Please write text');
+const two = 2;
+const result = userText.substr(Math.ceil(userText.length / two) - 1, two - userText.length % two);
+if(userText.length === 0 || !userText.trim()) {
+    alert('Invalid value')
+} else {
+    alert(`'${result}'`);
+}
+
+console.log(two - userText.length % two);
