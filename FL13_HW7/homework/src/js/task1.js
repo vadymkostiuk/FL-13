c071a9b2a71676c397951fa3aedac9221be7aa29
vadymkostyuk <kostiukvadym22@gmail.com>
2020-04-27 21:57:10 +0300

    add 1st task of 7_HW

--- FL13_HW7/homework/src/js/task1.js
+++ FL13_HW7/homework/src/js/task1.js
@@ -1,4 +1,5 @@
 const login = prompt('Please enter your Login');
+
 const user = 'User';
 const admin = 'Admin';
 const userPass = 'UserPass';
@@ -7,29 +8,36 @@ const maxLength = 4;
 const minDayHour = 8;
 const maxDayHour = 19;
 const date = new Date().getHours();
+
 if(login === null || login.length === 0 || !login.trim()) {
     alert('Canceled');
 } else if (login.length < maxLength){
     alert('I don\'t know any users having name length less than 4 symbols');
 } else if(login === user || login === admin) {
     const password = prompt('Please enter your Password');
+
     if(password === null || password.length === 0 || !password.trim()) {
         alert('Canceled');
     } else if(login === user && password === userPass) {
+
         if (date >= minDayHour && date <= maxDayHour) {
             alert('Good day, dear User!');
         } else {
             alert('Good evening, dear User!');
         }
+
     } else if(login === admin && password === adminPass) {
+
         if (date >= minDayHour && date <= maxDayHour) {
             alert('Good day, dear Admin!');
         } else {
             alert('Good evening, dear Admin!');
         }
+
     } else {
         alert('Wrong password');
     }
+
 } else {
     alert('I don’t know you');
 }
--- FL13_HW7/homework/src/js/task2.js
+++ FL13_HW7/homework/src/js/task2.js
@@ -1,1 +1,72 @@
-// Your code goes here
+const playGame = confirm('Do you want to play a game?');
+
+const maxAttempt = 3;
+const reducingWin = 2;
+const maxIteration = 5;
+const firstPrize = 100;
+const maxRandIter = 6;
+let maxRand = 6;
+let maxNumber = 5;
+let totalPrize = 0;
+
+let game = (nextPrize = firstPrize) => {
+    const random = +Math.round(Math.random() * maxRand);
+
+    for (let i = maxAttempt; i >= 1; i--) {
+        const userRandom = +prompt(`
+Choose a roulette pocket number from 0 to ${maxNumber}
+Attempts left: ${i}
+Total prize: ${totalPrize}$
+Possible prize on current attempt: ${nextPrize}$`);
+
+        if (userRandom === random) {
+            totalPrize += nextPrize;
+            let playCont = confirm(`Congratulation, you won! Your prize is: ${nextPrize}$. Do you want to continue?`);
+
+            if(playCont === true) {
+                maxRand += maxIteration;
+                maxNumber += maxIteration;
+
+                if (i === maxAttempt) {
+                    nextPrize *= reducingWin;
+                } else if (i === reducingWin) {
+                    nextPrize *= reducingWin * reducingWin;
+                } else {
+                    nextPrize *= reducingWin * reducingWin * reducingWin;
+                }
+
+                game(nextPrize);
+
+            } else {
+                alert(`Thank you for your participation. Your prize is: ${totalPrize}$`);
+                let playAgain = confirm('Do you want to play again?');
+
+                if(playAgain === true) {
+                    maxRand = maxRandIter;
+                    maxNumber = maxIteration;
+                    game();
+                }
+            }
+
+            break
+
+        } else if( i === 1 ) {
+            alert(`Thank you for your participation. Your prize is: ${totalPrize}$`);
+            let playAgain = confirm('Do you want to play again?');
+            if(playAgain === true) {
+                maxRand = maxRandIter;
+                maxNumber = maxIteration;
+                game();
+            }
+
+        } else {
+            nextPrize = nextPrize / reducingWin;
+        }
+    }
+};
+
+if(playGame === true) {
+    game();
+} else {
+    alert('You did not become a billionaire, but can.');
+}
